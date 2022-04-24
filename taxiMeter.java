8f8b4fdb4bf072bfcfc618a9d34f8a4cc58f9432
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-04-25 02:23:26 +0300

    Create taxiMeter.java

---
+++ taxiMeter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class taxiMeter {
+    public static void main(String[] args){
+        double price = 10,distance;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter distance(KM): ");
+        distance = input.nextDouble();
+        price = distance * 2.20;
+        System.out.println("Amount of the trip: "+ (price<20 ? 20 : price));
+
+    }
+}
