c41966bea3fb6a12e0a2baac71a52c08f6fd21b5
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-04-25 00:08:49 +0300

    Update kdvHesaplama.java

--- kdvHesaplama.java
+++ kdvHesaplama.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
-public class Main
+public class kdv
 {
     public static void main(String[] args) {
-   
-    Scanner input = new Scanner(System.in);
-    
-    System.out.print("KDV'siz Fiyat = ");
-    double taxFreeAmount = input.nextDouble();
-    System.out.println("KDV'li Fiyat = "+taxFreeAmount*1.18);
-    System.out.println("KDV Tutari = "+taxFreeAmount*0.18);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("KDV'siz Fiyat = ");
+        double taxFreeAmount = input.nextDouble();
+        if (taxFreeAmount<0){
+            System.out.println("Tutar sıfırdan küçük olamaz!\nSıfırdan büyük bir tutar giriniz...");
+        }
+        else {
+            double taxPlusPrice;
+            if (taxFreeAmount<=1000){
+                taxPlusPrice=taxFreeAmount*1.18;
+            }
+            else{
+                taxPlusPrice=taxFreeAmount*1.08;
+            }
+            System.out.println("KDV'li Fiyat = "+String.format("%.2f",taxPlusPrice));
+            System.out.println("KDV Tutari = "+String.format("%.2f",taxPlusPrice-taxFreeAmount));
+            }
     }
 }
