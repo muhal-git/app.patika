4c05a04e8afa49a7d22e648b90a875283dcccc85
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-05-22 23:48:22 +0300

    Create cinZodyagiBurcu.java

---
+++ cinZodyagiBurcu.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.util.*;
+
+
+public class cinZodyagiBurcu {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dogum yilinizi giriniz: ");
+        int dogumYili = Integer.parseInt(scanner.next());
+        switch ( dogumYili % 12 ) {
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Kopek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Okuz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavsan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yilan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+        }
+
+    }
+}
