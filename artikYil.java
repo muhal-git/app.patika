54b1fc09ab6b891537e2b8eb1c0f8c0b357b289c
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-05-23 00:00:02 +0300

    Create artikYil.java

---
+++ artikYil.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.*;
+
+
+public class artikYil {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz: ");
+        int yil = Integer.parseInt(scanner.next());
+
+        if ( yil % 100 == 0 && yil % 400 == 0 ) {
+            System.out.println(yil+" bir artık yıldır !");
+        } else if ( yil % 100 == 0 ) {
+            System.out.println(yil+" bir artık yıl değildir !");
+        } else if (yil % 4 == 0) {
+            System.out.println(yil + " bir artık yıldır !");
+        } else {
+            System.out.println(yil+" bir artık yıl değildir !");
+        }
+    }
+}
