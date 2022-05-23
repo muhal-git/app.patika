bb25323549aa0e857002e627ac8193e2e8473795
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-05-23 11:40:24 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Sayi giriniz: ");
+        Scanner scanner = new Scanner(System.in);
+        int sinirDeger = scanner.nextInt();
+        System.out.println("sinir deger: "+sinirDeger);
+    }
+}
