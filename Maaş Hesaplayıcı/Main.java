80a4b4967c98fbe7fe91f1ea8f1c16627845e5f6
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-07-18 01:36:43 +0300

    Create Main.java

---
+++ Maaş Hesaplayıcı/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee mami = new Employee("MAMİ",1000,45,1990);
+        
+        System.out.println("Maas:" + mami.getSalary());
+        
+    }
+}
