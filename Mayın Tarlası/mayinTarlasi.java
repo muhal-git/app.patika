ff036a7ca94bd52ce66df38c5aae6c12faab8135
muhal-git <73129339+muhal-git@users.noreply.github.com>
2022-08-07 19:47:41 +0300

    Create mayinTarlasi.java

---
+++ Mayın Tarlası/mayinTarlasi.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class mayinTarlasi {
+
+    public static void main(String[] args){
+        
+        MineSweeper mine = new MineSweeper();
+        mine.tarlaMayinDoldur();
+        mine.mayinTarlasiOyna();
+
+    }
+}
